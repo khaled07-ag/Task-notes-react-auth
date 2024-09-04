9eb1be587237b49ab28dd550b3d783de16e68185
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-04 18:20:16 +0300

    r

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -44,7 +44,7 @@ const Register = () => {
             <input
               type="text"
               id="name"
-              name="name"
+              name="username"
               onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
